46d67907234a7f483e599022715b21bc26fcdb2a
jeherr <johnherr@gmail.com>
2019-08-07 13:55:05 -0400

    models utils submodule added

--- rxntorch/models/utils/layers.py
+++ rxntorch/models/utils/layers.py
@@ -1,9 +1,9 @@
 import torch
+import torch.nn as nn
 
 from torch.nn.parameter import Parameter
-from torch.nn.modules.module import Module
 
-class GraphConvolution(Module):
+class GraphConvolution(nn.Module):
     """
     Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
     """
@@ -37,3 +37,17 @@ class GraphConvolution(Module):
         return self.__class__.__name__ + ' (' \
                + str(self.in_features) + ' -> ' \
                + str(self.out_features) + ')'
+
+class LayerNorm(nn.Module):
+    "Construct a layernorm module (See citation for details)."
+
+    def __init__(self, features, eps=1e-6):
+        super(LayerNorm, self).__init__()
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
+        self.eps = eps
+
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
