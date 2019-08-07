bd17cd448d5d73d9018cbbcb38447e6b6a14a764
jeherr <johnherr@gmail.com>
2019-08-07 13:46:20 -0400

    restructuring models into submodule

--- rxntorch/models/network.py
+++ rxntorch/models/network.py
@@ -24,3 +24,16 @@ class GCN(nn.Module):
 
     def forward(self, x, adj):
 
+class Transformer(nn.module):
+    def __init__(self, dataset, batch_size, pre_proc_threads=1):
+        super(Transformer, self).__init__()
+        self.dataset = dataset
+        self.batch_size = batch_size
+        self.sampler = BinRandomSampler(self.dataset.bins, self.batch_size)
+        self.data_loader = DataLoader(self.dataset,
+                                    batch_size=self.batch_size,
+                                    num_workers=pre_proc_threads,
+                                    sampler=self.sampler)
+
+    def forward(self, x, adj):
+
--- rxntorch/utils.py
+++ rxntorch/utils.py
@@ -2,6 +2,9 @@ from __future__ import print_function
 
 import random
 import numpy as np
+import torch
+import torch.nn as nn
+import math
 
 import rdkit.Chem as Chem
 
@@ -101,3 +104,12 @@ def bond_features(bond):
                 bt == Chem.rdchem.BondType.AROMATIC,
                 bond.GetIsConjugated(),
                 bond.IsInRing()], dtype=np.float32)
+
+
+class GELU(nn.Module):
+    """
+    Paper Section 3.4, last paragraph notice that BERT used the GELU instead of RELU
+    """
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(math.sqrt(2 / math.pi) * (x + 0.044715 * torch.pow(x, 3))))
