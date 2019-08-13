af87a0c4067477942ae433aea7064e9aeb5cbf46
jeherr <johnherr@gmail.com>
2019-08-13 11:19:40 -0400

    collapsed embedding to one file

--- rxntorch/models/embedding.py
+++ rxntorch/models/embedding.py
@@ -1,8 +1,6 @@
+import torch
+import math
 import torch.nn as nn
-from .token import TokenEmbedding
-from .position import PositionalEmbedding
-from .segment import SegmentEmbedding
-
 
 class BERTEmbedding(nn.Module):
     """
@@ -22,7 +20,7 @@ class BERTEmbedding(nn.Module):
         """
         super().__init__()
         self.token = TokenEmbedding(vocab_size=vocab_size, embed_size=embed_size)
-        self.position = PositionalEmbedding(d_model=self.token.embedding_dim)
+        self.position = PositionalEmbedding(model_dim=self.token.embedding_dim)
         self.segment = SegmentEmbedding(embed_size=self.token.embedding_dim)
         self.dropout = nn.Dropout(p=dropout)
         self.embed_size = embed_size
@@ -30,3 +28,36 @@ class BERTEmbedding(nn.Module):
     def forward(self, sequence, segment_label):
         x = self.token(sequence) + self.position(sequence) + self.segment(segment_label)
         return self.dropout(x)
+
+
+class TokenEmbedding(nn.Embedding):
+    def __init__(self, vocab_size, embed_size=512):
+        super().__init__(vocab_size, embed_size, padding_idx=0)
+
+
+class PositionalEmbedding(nn.Module):
+
+    def __init__(self, d_model, max_len=512):
+        super().__init__()
+
+        # Compute the positional encodings once in log space.
+        pe = torch.zeros(max_len, d_model).float()
+        pe.require_grad = False
+
+        position = torch.arange(0, max_len).float().unsqueeze(1)
+        div_term = (torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)).exp()
+
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        return self.pe[:, :x.size(1)]
+
+
+class SegmentEmbedding(nn.Embedding):
+    def __init__(self, embed_size=512):
+        super().__init__(3, embed_size, padding_idx=0)
+
