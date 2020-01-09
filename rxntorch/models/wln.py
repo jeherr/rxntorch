2201c4de721129bca5de26f66ee2b16592b1b905
jeherr <johnherr@gmail.com>
2020-01-09 15:44:29 -0500

    starting bond feature updates in WLN

--- rxntorch/models/wln.py
+++ rxntorch/models/wln.py
@@ -7,29 +7,31 @@ class WLNet(nn.Module):
     def __init__(self, depth, afeats_size, bfeats_size, hidden_size):
         super(WLNet, self).__init__()
         self.depth = depth
-        self.fc1 = Linear(afeats_size, hidden_size, bias=False)
-        self.graph_conv_nei = Linear(hidden_size + bfeats_size, hidden_size)
+        self.fc1a = Linear(afeats_size, hidden_size, bias=False)
+        self.fc1b = Linear(afeats_size, hidden_size, bias=False)
+        self.graph_conv_nei = Linear(hidden_size * 2, hidden_size)
         self.graph_conv_atom = Linear(hidden_size * 2, hidden_size)
         self.fc2atom_nei = Linear(hidden_size, hidden_size, bias=False)
         self.fc2bond_nei = Linear(bfeats_size, hidden_size, bias=False)
         self.fc2 = Linear(hidden_size, hidden_size, bias=False)
 
     def forward(self, atom_feats, bond_feats, atom_graph, bond_graph, num_nbs, n_atoms, mask_neis, mask_atoms):
-        atom_feats = F.relu(self.fc1(atom_feats))
-        bondnei_feats = torch.stack([bond_feats[i,bond_graph[i],:] for i in range(atom_feats.shape[0])])
+        atom_feats = F.relu(self.fc1a(atom_feats))
+        bond_feats = F.relu(self.fc1b(bond_feats))
         for i in range(self.depth):
-            atomnei_feats = torch.stack([atom_feats[i,atom_graph[i],:] for i in range(atom_feats.shape[0])])
+            atom_neighs = torch.stack([atom_feats[i,atom_graph[i],:] for i in range(atom_feats.shape[0])])
+            bond_neighs = torch.stack([bond_feats[i,bond_graph[i],:] for i in range(atom_feats.shape[0])])
             if (i + 1) == self.depth:
-                atomnei_feats = self.fc2atom_nei(atomnei_feats)
-                bondnei_feats = self.fc2bond_nei(bondnei_feats)
+                atomnei_feats = self.fc2atom_nei(atom_neighs)
+                bondnei_feats = self.fc2bond_nei(bond_neighs)
                 nei_feats = atomnei_feats * bondnei_feats
                 nei_feats = torch.where(mask_neis, nei_feats, torch.zeros_like(nei_feats)).sum(-2)
                 self_feats = self.fc2(atom_feats)
                 local_feats = self_feats * nei_feats
                 local_feats = torch.where(mask_atoms, local_feats, torch.zeros_like(local_feats))
             else:
-                nei_feats = F.relu(self.graph_conv_nei(torch.cat([atomnei_feats, bondnei_feats], dim=-1)))
-                nei_feats = torch.where(mask_neis, nei_feats, torch.zeros_like(nei_feats)).sum(-2)
-                update_feats = torch.cat([atom_feats, nei_feats], dim=-1)
-                atom_feats = F.relu(self.graph_conv_atom(update_feats))
+                atom_neigh_feats = F.relu(self.graph_conv_nei(torch.cat([atom_neighs, bond_neighs], dim=-1)))
+                atom_neigh_feats = torch.where(mask_neis, atom_neigh_feats, torch.zeros_like(atom_neigh_feats)).sum(-2)
+                atom_updates = torch.cat([atom_feats, atom_neigh_feats], dim=-1)
+                atom_feats = F.relu(self.graph_conv_atom(atom_updates))
         return local_feats
