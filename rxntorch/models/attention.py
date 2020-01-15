d75787b92e7e489c217f7078392094866e1bc870
jeherr <johnherr@gmail.com>
2020-01-15 11:58:40 -0500

    merging all sub layers

--- rxntorch/models/attention.py
+++ rxntorch/models/attention.py
@@ -7,14 +7,14 @@ from .layers import Linear
 class Attention(nn.Module):
     def __init__(self, hidden_size, binary_size):
         super(Attention, self).__init__()
-        self.fcapair = Linear(hidden_size, hidden_size, bias=False)
-        self.fcbinary = Linear(binary_size, hidden_size)
-        self.fcattention = Linear(hidden_size, 1)
+        self.fc_attention1 = Linear(hidden_size + binary_size, hidden_size)
+        self.fc_attention = Linear(hidden_size, 1)
 
     def forward(self, local_feats, binary_feats, sparse_idx):
         local_pair = local_feats.unsqueeze(1) + local_feats.unsqueeze(2)
-        attention_features = F.relu(self.fcapair(local_pair) + self.fcbinary(binary_feats))
-        attention_score = torch.sigmoid(self.fcattention(attention_features))
+        all_feats = torch.cat([local_pair, binary_feats],dim=-1)
+        attention_features = F.relu(self.fc_attention1(all_feats))
+        attention_score = torch.sigmoid(self.fc_attention(attention_features))
         global_feats = torch.sum(local_feats.unsqueeze(1) * attention_score, dim=2)
         global_pair1 = global_feats[sparse_idx[:,0],sparse_idx[:,1]]
         global_pair2 = global_feats[sparse_idx[:,0],sparse_idx[:,2]]
--- rxntorch/models/wln.py
+++ rxntorch/models/wln.py
@@ -7,12 +7,11 @@ class WLNet(nn.Module):
     def __init__(self, depth, afeats_size, bfeats_size, hidden_size):
         super(WLNet, self).__init__()
         self.depth = depth
-        self.fc1 = Linear(afeats_size, hidden_size, bias=False)
+        self.fc1 = Linear(afeats_size, hidden_size)
         self.graph_conv_nei = Linear(hidden_size + bfeats_size, hidden_size)
         self.graph_conv_atom = Linear(hidden_size * 2, hidden_size)
-        self.fc2atom_nei = Linear(hidden_size, hidden_size, bias=False)
-        self.fc2bond_nei = Linear(bfeats_size, hidden_size, bias=False)
-        self.fc2 = Linear(hidden_size, hidden_size, bias=False)
+        self.fc2_nei = Linear(hidden_size + bfeats_size, hidden_size)
+        self.fc2 = Linear(hidden_size, hidden_size)
 
     def forward(self, atom_feats, bond_feats, atom_graph, bond_graph, num_nbs, n_atoms, mask_neis, mask_atoms):
         atom_feats = F.relu(self.fc1(atom_feats))
@@ -20,9 +19,8 @@ class WLNet(nn.Module):
         for i in range(self.depth):
             atomnei_feats = torch.stack([atom_feats[i,atom_graph[i],:] for i in range(atom_feats.shape[0])])
             if (i + 1) == self.depth:
-                atomnei_feats = self.fc2atom_nei(atomnei_feats)
-                bondnei_feats = self.fc2bond_nei(bondnei_feats)
-                nei_feats = atomnei_feats * bondnei_feats
+                all_feats = torch.cat([atomnei_feats, bondnei_feats], dim=-1)
+                nei_feats = self.fc2_nei(all_feats)
                 nei_feats = torch.where(mask_neis, nei_feats, torch.zeros_like(nei_feats)).sum(-2)
                 self_feats = self.fc2(atom_feats)
                 local_feats = self_feats * nei_feats
