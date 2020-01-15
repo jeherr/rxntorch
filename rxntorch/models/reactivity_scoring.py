04e2be224fdce82d89efdbae0a168c7cadf7c2a7
jeherr <johnherr@gmail.com>
2020-01-15 10:57:31 -0500

    merging some separate fc layers

--- rxntorch/models/reactivity_scoring.py
+++ rxntorch/models/reactivity_scoring.py
@@ -7,14 +7,13 @@ from .layers import Linear
 class ReactivityScoring(nn.Module):
     def __init__(self, hidden_size, binary_size):
         super(ReactivityScoring, self).__init__()
-        self.fclocal = Linear(hidden_size, hidden_size, bias=False)
-        self.fcglobal = Linear(hidden_size, hidden_size)
-        self.fcbinary = Linear(binary_size, hidden_size, bias=False)
+        self.fcall = Linear(hidden_size * 2 + binary_size, hidden_size)
         self.fcscore = Linear(hidden_size, 5)
 
-    def forward(self, local_pair, global_pair, binary, sparse_idx):
-        binary_feats = binary[sparse_idx[:,0],sparse_idx[:,1],sparse_idx[:,2]]
-        pair_feats = F.relu(self.fclocal(local_pair) + self.fcglobal(global_pair) + self.fcbinary(binary_feats))
+    def forward(self, local_pair, global_pair, binary_feats, sparse_idx):
+        binary_feats = binary_feats[sparse_idx[:,0],sparse_idx[:,1],sparse_idx[:,2]]
+        all_feats = torch.cat([local_pair, global_pair, binary_feats], dim=-1)
+        pair_feats = F.relu(self.fcall(all_feats))
         score = self.fcscore(pair_feats)
         return score
 
